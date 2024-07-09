0c298f97284ebb06d99f9a30a8c407591ae3700e
Kelvin Ward <kelvinward1010@gmail.com>
2024-07-10 00:03:30 +0700

    update user

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
+from datetime import datetime
 
 class CreateUser(BaseModel):
     name: str
@@ -7,3 +8,7 @@ class CreateUser(BaseModel):
     password: str
     image: str | None = None
     position: str | None = None
+
+class CreateUserFinal(CreateUser):
+    createdAt: datetime
+    updatedAt: datetime
--- app/services/user.py
+++ app/services/user.py
@@ -1,19 +1,30 @@
 from fastapi import APIRouter, HTTPException, status, Response
-from ..db import init_db
 from bson import ObjectId
-
+from datetime import datetime
+from ..db import init_db
+from ..utils import init_util
+from ..schemas import user
 
 db = init_db.users_collection
 
-async def create_user(user):
-    created_user = db.insert_one(dict(user))
-    user_ok = db.find_one({"_id": ObjectId(created_user.inserted_id)})
-    convert = Entity(user_ok)
+async def create_user(user_info):
+    
+    hashed_password = init_util.hash_password(user_info['password'])
+    user_info['password'] = hashed_password
+    
+    data_to_create = user.CreateUserFinal(**user_info, createdAt=datetime.now(), updatedAt=datetime.now())
+    
+    if hashed_password:
+        create_user = db.insert_one(dict(data_to_create))
+        
+    user_created = db.find_one({"_id": ObjectId(create_user.inserted_id)})
+    
+    converted = Entity(user_created)
     
     return {
         "status": 201,
         "message": "success",
-        "data": convert
+        "data": converted
     }
 
 
@@ -26,4 +37,6 @@ def Entity(user) -> dict:
         "password": user["password"],
         "image": user["image"],
         "position": user["position"],
+        "createdAt": user["createdAt"],
+        "updatedAt": user["updatedAt"],
     }
